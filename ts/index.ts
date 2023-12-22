e5a55e2b994f69fa92e46d98b7775bf6e8546182
omar <omarjab2411@gmail.com>
2023-12-22 23:13:10 +0100

    Versione 1 Restored

--- ts/index.ts
+++ ts/index.ts
@@ -1,15 +1,4 @@
 let a: string = "omar"
 
-type Persona = {
-    nome: string,
-    eta: number,
-    email: string
-}
 
-const p1: Persona = {
-    nome: 'omar',
-    eta: 22,
-    email: 'omarjab01@gmail.com'
-}
-
-console.log(p1)
+console.log(a)
