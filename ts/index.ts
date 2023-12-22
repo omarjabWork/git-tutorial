e713c807852dfe3a295ed84984fc31fec6bcb3e7
omar <omarjab2411@gmail.com>
2023-12-22 22:55:56 +0100

    Secondo Commit

--- ts/index.ts
+++ ts/index.ts
@@ -1,4 +1,15 @@
 let a: string = "omar"
 
+type Persona = {
+    nome: string,
+    eta: number,
+    email: string
+}
 
-console.log(a)
+const p1: Persona = {
+    nome: 'omar',
+    eta: 22,
+    email: 'omarjab01@gmail.com'
+}
+
+console.log(p1)
